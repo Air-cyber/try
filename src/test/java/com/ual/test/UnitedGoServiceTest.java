d6aebdf3e6d73b58512f3689769b1e5882c89d8b
u435748 <u435748@L0007577.global.ual.com>
2025-09-05 15:06:51 +0530

    "Pom.xml"

--- src/test/java/com/ual/test/UnitedGoServiceTest.java
+++ src/test/java/com/ual/test/UnitedGoServiceTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
